d535619324229e0ed698657a2ff237b33ddb3fbf
Leandro Almeida <leandro_almeida@id.uff.br>
2022-05-27 23:13:07 -0300

    format files

--- src/dto/update-manager-employee.input.ts
+++ src/dto/update-manager-employee.input.ts
@@ -1,6 +1,8 @@
 import { CreateManagerEmployeeInput } from './create-manager-employee.input';
 import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateManagerEmployeeInput extends PartialType(CreateManagerEmployeeInput) {
+export class UpdateManagerEmployeeInput extends PartialType(
+  CreateManagerEmployeeInput,
+) {
   id: number;
 }
--- src/dto/update-users-integration.input.ts
+++ src/dto/update-users-integration.input.ts
@@ -1,6 +1,8 @@
 import { CreateUsersIntegrationInput } from './create-users-integration.input';
 import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateUsersIntegrationInput extends PartialType(CreateUsersIntegrationInput) {
+export class UpdateUsersIntegrationInput extends PartialType(
+  CreateUsersIntegrationInput,
+) {
   id: number;
 }

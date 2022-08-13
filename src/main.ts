2c9f1612127283e319fdfe3546d1631512160364
Leandro Almeida <leandro_almeida@id.uff.br>
2022-08-13 12:14:38 -0300

    try withdount dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import 'dotenv/config';
+// import 'dotenv/config';
 
 async function bootstrap() {
-  const port = process.env.port || 3000;
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }

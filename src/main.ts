7a390b87a81b2ae35801f017473a2b1c89cceeba
Leandro Almeida <leandro_almeida@id.uff.br>
2022-08-13 12:06:35 -0300

    add port and node version

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './modules/app.module';
 import 'dotenv/config';
 
 async function bootstrap() {
+  const port = process.env.port || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
